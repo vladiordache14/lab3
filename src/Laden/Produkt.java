3c562babf0cbf8eb9524d9dd1227beb23d0abd34
vladi <vladiordache2002@yahoo.com>
2022-10-18 11:39:09 +0300

    a

--- src/Laden/Produkt.java
+++ src/Laden/Produkt.java
@@ -1,2 +1,53 @@
-package Laden;public class Produkt {
+package Laden;
+
+import java.util.*;
+
+import static java.lang.Math.floor;
+
+public class Produkt {
+    private int date;
+    private float Preis;
+    private String Name;
+
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public float getPreis() {
+        return Preis;
+    }
+
+
+    public void setPreis(float Preis) {
+        if((Preis/0.05d) % 1 == 0) {
+            this.Preis=Preis;
+        }
+        else
+        {
+            this.Preis = (float) ((floor(Preis/0.05d)+1)*0.05d);
+        }
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Produkt(String Name, int date, float Preis){
+        this.date=date;
+        setPreis(Preis);
+        this.Name=Name;
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import Laden.Produkt;
 public class Main {
+
+    public static void printList(ArrayList<Produkt> lista)
+    {
+        for(Produkt p: lista)
+        {
+            System.out.println(p.getDate());
+            System.out.println(p.getName());
+            System.out.println(p.getPreis());
+        }
+    }
+    public static void update(ArrayList<Produkt> lista){
+        for (Produkt produkt : lista) {
+            if (produkt.getDate() > 10)
+                produkt.setDate(produkt.getDate() - produkt.getDate() / 10);
+            if (produkt.getDate() > 20)
+                produkt.setPreis(produkt.getPreis() - produkt.getPreis() * 2 / 10);
+        }
+    }
+    public static ArrayList<Produkt> teuer(ArrayList<Produkt> lista){
+        ArrayList<Produkt> teuereProdukte = new ArrayList<>();
+        for(Produkt produkt : lista)
+        {
+            if(produkt.getPreis() >100) {
+
+                teuereProdukte.add(produkt);
+            }
+        }
+        return teuereProdukte;
+    }
+
+    public static ArrayList<Produkt> sortare(ArrayList<Produkt> lista){
+        update(lista);
+        lista.sort(Comparator.comparing(Produkt::getPreis));
+        return lista;
+    }
+
+    public static Produkt teuerste(ArrayList<Produkt> lista)
+    {
+        float max=0;
+        Produkt aux = new Produkt("a",0 ,0);
+        for(Produkt produkt : lista){
+            if(produkt.getPreis() >max) {
+                max = produkt.getPreis();
+                aux=produkt;
+            }
+        }
+        return aux;
+    }
+    public static Produkt fresh(ArrayList<Produkt> lista)
+    {
+        int min=9999;
+        Produkt aux = new Produkt("a",0 ,0);
+        for(Produkt produkt : lista){
+            if(produkt.getDate() <min) {
+                min = produkt.getDate();
+                aux=produkt;
+            }
+        }
+        return aux;
+    }
+    public static void printProduct(Produkt p)
+    {
+        System.out.println(p.getName());
+        System.out.println(p.getDate());
+        System.out.println(p.getPreis());
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Produkt> a=new ArrayList<>();
+        a.add(new Produkt("Salami",13,70));
+        a.add(new Produkt("ranzxa",3,70));
+        a.add(new Produkt("Tomato",23,700));
+        printList(sortare(a));
+        printProduct(fresh(a));
+        printProduct(teuerste(a));
+
+
     }
 }
